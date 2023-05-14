c46df717211fe21d003cc7aec9213bbdcf484680
david perez <elquinceminutos@hotmail.com>
2023-05-14 14:44:46 -0500

    Add files via upload

---
+++ src/Components/AddProducts.js
@@ -0,0 +1,124 @@
+import React,{useState} from 'react'
+import {storage,fs} from '../Config/Config'
+
+export const AddProducts = () => {
+
+    const [title, setTitle]=useState('');
+    const [description, setDescription]=useState('');
+    const [price, setPrice]=useState('');
+    const [category, setCategory]=useState('');
+    const [image, setImage]=useState(null);
+
+    const [imageError, setImageError]=useState('');
+    
+    const [successMsg, setSuccessMsg]=useState('');
+    const [uploadError, setUploadError]=useState('');
+
+    const types =['image/jpg','image/jpeg','image/png','image/PNG'];
+    const handleProductImg=(e)=>{
+        let selectedFile = e.target.files[0];
+        if(selectedFile){
+            if(selectedFile&&types.includes(selectedFile.type)){
+                setImage(selectedFile);
+                setImageError('');
+            }
+            else{
+                setImage(null);
+                setImageError('Favor selecciones una imagen valida (png or jpg)')
+            }
+        }
+        else{
+            console.log('Seleccione archivo');
+        }
+    }
+    const handleAddProducts=(e)=>{
+        e.preventDefault();
+        // console.log(title, description, price);
+        // console.log(image);
+        const uploadTask=storage.ref(`product-images/${image.name}`).put(image);
+        uploadTask.on('state_changed',snapshot=>{
+            const progress = (snapshot.bytesTransferred/snapshot.totalBytes)*100
+            console.log(progress);
+        },error=>setUploadError(error.message),()=>{
+            storage.ref('product-images').child(image.name).getDownloadURL().then(url=>{
+                fs.collection('Products').add({
+                    title,
+                    description,
+                    category,
+                    price: Number(price),
+                    url
+                }).then(()=>{
+                    setSuccessMsg('Su producto se agregó correctamente');
+                    setTitle('');
+                    setDescription('');
+                    setCategory('');
+                    setPrice('');
+                    document.getElementById('file').value='';
+                    setImageError('');
+                    setUploadError('');
+                    setTimeout(()=>{
+                        setSuccessMsg('');
+                    },3000)
+                }).catch(error=>setUploadError(error.message));
+            })
+        })
+    }
+  
+    return (
+        <div className='container'>
+            <br></br>
+            <br></br>
+            <h1>Agregar producto</h1>
+            <hr></hr>        
+            {successMsg&&<>
+                <div className='success-msg'>{successMsg}</div>
+                <br></br>
+            </>} 
+            <form autoComplete="off" className='form-group' onSubmit={handleAddProducts}>
+                <label>Titulo del producto</label>
+                <input type="text" className='form-control' required
+                onChange={(e)=>setTitle(e.target.value)} value={title}></input>
+                <br></br>
+                <label>Descripcion del producto</label>
+                <input type="text" className='form-control' required
+                onChange={(e)=>setDescription(e.target.value)} value={description}></input>
+                <br></br>
+                <label>Precio del producto</label>
+                <input type="number" className='form-control' required
+                onChange={(e)=>setPrice(e.target.value)} value={price}></input>
+                <br></br>
+                <label>Categorias</label>
+                <select className='form-control' required
+                value={category} onChange={(e)=>setCategory(e.target.value)}>                                    
+                    <option value="">Selecionar la categoria del producto</option>                   
+                    <option>Alimento</option>
+                    <option>Ropa para cahorros</option>
+                    <option>Juguetes</option>
+                    <option>Pet lovers</option>
+                   </select>
+                <br></br>
+                <label>Cargar imagen del producto</label>
+                <input type="file" id="file" className='form-control' required
+                onChange={handleProductImg}></input>
+                
+                {imageError&&<>
+                    <br></br>
+                    <div className='error-msg'>{imageError}</div>
+                   
+                </>}
+                <br></br>           
+                <div style={{display:'flex', justifyContent:'flex-end'}}>
+                    <button type="submit" className='btn btn-success btn-md'>
+                        Enviar
+                    </button>
+                </div>
+            </form>
+            {uploadError&&<>
+                    <br></br>
+                    <div className='error-msg'>{uploadError}</div>
+                    
+                </>}
+
+        </div>
+    )
+}
